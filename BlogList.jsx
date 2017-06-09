7ba06422b8ead26c72a6c4a8edcae12d8d464ce4
Alexander <sitesmake@gmail.com>
2017-06-09 16:07:24 +0300

    lesson2 exercise

--- BlogList.jsx
+++ BlogList.jsx
@@ -1,27 +1,93 @@
-const DOM = React.DOM;
+const { DOM, PropTypes } = React;
 
-const Image = (props) => (
-  DOM.img({
-    src: props.src,
-    width: props.width,
-    height: props.height,
-    alt: props.alt
-    }, null)
-);
+const { bind } = _;
+
+class Image extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return(
+      DOM.img({
+        src: this.props.src,
+        width: (this.props.src.split('/')[3] || this.props.width),
+        height: (this.props.src.split('/')[4] || this.props.height),
+        alt: this.props.alt
+      }, null)
+    )
+  }
+}
+
+Image.defaultProps = {
+  src: 'http://lorempixel.com/50/50',
+  width: 50,
+  height: 50,
+  alt: 'Lorem Ipsum'
+}
+
+Image.propTypes = {
+  src: PropTypes.string,
+  width: PropTypes.number,
+  height: PropTypes.number,
+  alt: PropTypes.string
+}
 
 const TextBox = (props) => (
   DOM.span(null, props.children)
 );
 
 class BlogItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { likes: this.props.meta.likes };
+
+    this.handleClick = bind(this.handleClick, this);
+  }
+
+  handleClick() {
+    this.setState({ likes: this.state.likes + 1 })
+  }
+
   render() {
     return (
       <div>
         <Image src={this.props.src} />
-        <TextBox>{this.props.text}</TextBox>
+        <TextBox>
+          {this.props.text},
+          by: {this.props.meta.author},
+          created at: {this.props.meta.createdAt},
+          updated at: {this.props.meta.updatedAt},
+          likes: {this.state.likes}
+          <button onClick={this.handleClick}>Like</button>
+        </TextBox>
       </div>
     )
   }
+}
+
+BlogItem.defaultProps = {
+  src: 'http://lorempixel.com/50/50',
+  text: 'Untitled Blog',
+  id: Date.now(),
+  meta: {
+    author: 'Me',
+    createdAt: moment().format('lll'),
+    updatedAt: moment().format('lll'),
+    likes: 0
+  }
+}
+
+BlogItem.propTypes = {
+  src: PropTypes.string,
+  text: PropTypes.string,
+  id: PropTypes.number,
+  meta: PropTypes.shape({
+    author: PropTypes.string,
+    createdAt: PropTypes.string,
+    updatedAt: PropTypes.string,
+    likes: PropTypes.number
+  })
 }
 
 class BlogList extends React.Component {
@@ -31,7 +97,9 @@ class BlogList extends React.Component {
       _.map(
         this.props.items,
         (item) => (
-          <li key={item.id}><BlogItem src={item.src} text={item.text} /></li>
+          <li key={item.id}>
+            <BlogItem src={item.src} text={item.text} meta={item.meta} />
+          </li>
         )
       )
     )
@@ -42,17 +110,35 @@ const blogItems = [
   {
     src: 'http://lorempixel.com/200/200',
     text: 'Blog 1',
-    id: 1
+    id: 1,
+    meta: {
+      author: 'Philosopher',
+      createdAt: moment().subtract(2000, 'years').format('lll'),
+      updatedAt: moment().format('lll'),
+      likes: 2000
+    }
   },
   {
     src: 'http://lorempixel.com/200/100',
     text: 'Blog 2',
-    id: 2
+    id: 2,
+    meta: {
+      author: 'Knight',
+      createdAt: moment().subtract(500, 'years').format('lll'),
+      updatedAt: moment().format('lll'),
+      likes: 500
+    }
   },
   {
     src: 'http://lorempixel.com/200/50',
     text: 'Blog 3',
-    id: 3
+    id: 3,
+    meta: {
+      author: 'Physicist',
+      createdAt: moment().subtract(200, 'years').format('lll'),
+      updatedAt: moment().format('lll'),
+      likes: 200
+    }
   }
 
 ];
