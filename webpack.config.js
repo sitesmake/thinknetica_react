27875120f2d0ff05a0701ada6c3670b9bf4e0661
Alexander <sitesmake@gmail.com>
2017-06-25 10:52:34 +0300

    ready for webpack

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+/* eslint-disable */
+
+var path = require('path');
+
+var webpack = require('webpack');
+
+var root = path.join(process.cwd(), 'src');
+
+module.exports = {
+  entry: [
+    './src/index.js'
+  ],
+
+  output: {
+    path: path.join(process.cwd(), 'dist'),
+    publicPath: '/assets/',
+    fileName: 'bundle.js'
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+
+  resolve: {
+    root: root
+  }
+};
