e8a78ef4c2a30f32841e647609b25ae01386dd25
Alexander <sitesmake@gmail.com>
2017-06-26 19:22:31 +0300

    blog page linted

--- src/components/BlogPage.js
+++ src/components/BlogPage.js
@@ -2,6 +2,10 @@ import React from 'react';
 
 import BlogList from 'components/widgets/blog/BlogList';
 
+import _ from 'lodash';
+
+const { PropTypes }  = React;
+
 class BlogPage extends React.Component {
   constructor(props) {
     super(props);
@@ -11,24 +15,32 @@ class BlogPage extends React.Component {
 
   incrementLikes(id) {
     // let copy = Object.assign({}, this.state.posts);
-    let copy = _.cloneDeep(this.state.posts);
+    const copy = _.cloneDeep(this.state.posts);
 
-    const index = copy.findIndex(item => item.id == id)
+    const index = copy.findIndex(item => item.id == id);
 
-    copy[index]["meta"]["likes"]++;
+    copy[index]['meta']['likes']++;
 
     this.setState({
       posts: copy
-    })
+    });
   }
 
   render() {
-    return(
+    return (
       <div>
-        <BlogList items={ this.state.posts } incrementLikes={ this.incrementLikes } />
+        <BlogList
+          items={ this.state.posts }
+          incrementLikes={ this.incrementLikes }
+        />
       </div>
-    )
+    );
   }
 }
+
+BlogPage.propTypes = {
+  posts: PropTypes.array,
+  incrementLikes: PropTypes.func,
+};
 
 export default BlogPage;
